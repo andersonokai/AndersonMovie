bf4e74871007958e6cb020870086bdd2d8423df7
febirison <andersonokai@gmail.com>
2025-09-20 03:47:34 +0000

    feat: Seed database with initial movie data
    Introduce `SeedData.cs` to populate the database with sample movie entries.
    Integrate the seeding logic into `Program.cs` to run on application startup,
    ensuring the database has initial data if it's empty.

--- obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93833b4c2ec2501d6c497239f2c071763ba7ef0c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3872e9532420761c91839a73b20e88803a092de7")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
