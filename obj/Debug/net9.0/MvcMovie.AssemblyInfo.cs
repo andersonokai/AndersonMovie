3872e9532420761c91839a73b20e88803a092de7
febirison <andersonokai@gmail.com>
2025-09-20 03:37:53 +0000

    Update build artifacts and local database files.

--- obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+696a0aae7a49e945273b89c8331512228cf89d7a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93833b4c2ec2501d6c497239f2c071763ba7ef0c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
